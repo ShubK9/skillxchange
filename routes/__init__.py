1364d9e9c416688d86a1be91bfa2d9e3337808a1
shubk9 <sk311h@gmail.com>
2025-11-27 06:26:38 +0530

    init

--- routes/__init__.py
+++ routes/__init__.py
@@ -6,7 +6,6 @@ from .sessions import router as sessions_router
 from .ratings import router as ratings_router
 from .signaling import router as signaling_router
 from .teachers import router as teachers_router
-from .chat import chat_router  
 
 __all__ = [
     "auth_router",
@@ -16,7 +15,4 @@ __all__ = [
     "ratings_router",
     "signaling_router",
     "teachers_router",
-    "chat_router",  
 ]
-
-
